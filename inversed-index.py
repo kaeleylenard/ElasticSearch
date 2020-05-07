8704241251d717c8f5861872721fd510f63e669a
areetaw <areetaw@gmail.com>
2020-05-06 22:17:19 -0700

    computed tf score and changed posting into a set

--- inversed-index.py
+++ inversed-index.py
@@ -7,7 +7,7 @@ import nltk
 # nltk.download('punkt')
 from nltk.stem import PorterStemmer
 
-# UNCOMMENT THIS BASED ON WHO'S COMPUTER IS BEING USED
+# UNCOMMENT THIS BASED ON WHOSE COMPUTER IS BEING USED
 
 # Kaeley:
 #dev_directory = '/Users/kaeleylenard/Documents/CS121-Spring2020/Assignment3/DEV'
@@ -45,17 +45,19 @@ def tokenizes(data):
     return tokens
 
 
-def computeTF():
-    # TO-DO
+def compute_tf(token, tokens):
+    # term_freq(t,d) = count of t in d / number of words in d
+    amount_of_token_in_tokens = tokens.count(token) * 1.0
+    length_of_tokens = len(tokens) * 1.0
+    return amount_of_token_in_tokens/length_of_tokens
+
+
+def compute_idf():
+    # inverse_doc_freq(t) = log(N/(df + 1))
     return 0.0
 
 
-def computeIDF():
-    # TO-DO
-    return 0.0
-
-
-def computeTFIDF():
+def compute_tfidf():
     # TO-DO: formula is tf-idf(t, d) = tf(t, d) * log(N/(df + 1))
     return 0.0
 
@@ -64,34 +66,28 @@ def add_to_index(document_words, docid_counter):
     # value should document name/id token was found in
     # and tf-idf score (need to compute)
     for word in document_words:
-        amount_of_times_found_in_doc = document_words.count(word)
-        # find later
-        # tfidf_score = 0.0
+        tfidf_score = 0.0
         if word not in inverse_index:
-            inverse_index[word] = [{"Document ID": [docid_counter]}, amount_of_times_found_in_doc]
+            inverse_index[word] = set((docid_counter, tfidf_score))
         else:
-            inverse_index[word][0]["Document ID"].append(docid_counter)
+            inverse_index.append((docid_counter, tfidf_score))
 
 
 for subdir, dirs, files in os.walk(dev_directory):
     for file in files:
         docid_counter += 1
-        # document_words = set()
         json_file = os.path.join(subdir, file)
-
+        alphanumeric_sequences = []
+        print("current file:", json_file)
         try:
             soup = BeautifulSoup(open(json_file), 'html.parser')
             for text in soup.findAll(["title", "p", "b", re.compile('^h[1-6]$')]):
                 data = text.get_text().strip()
-
-                # has duplicates to check variance of appearance in doc
-                alphanumeric_sequences = tokenizes(data)
-                # document_words |= alphanumeric_sequences
-
+                alphanumeric_sequences += tokenizes(data)
             add_to_index(alphanumeric_sequences, docid_counter)
-            print("current file:", json_file)
         except Exception as e:
-            print("error at", e)
+            print("error at:", e)
+
 
 # for deliverable 1
 deliverable_text = open('/Users/AreetaW/Desktop/deliverable.txt', 'w')
