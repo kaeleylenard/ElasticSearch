5676ef98b7c1ceee3c38a3e2504daca056faecf9
areetaw <areetaw@gmail.com>
2020-05-11 13:41:10 -0700

    added tfidf calculations

--- inversed-index.py
+++ inversed-index.py
@@ -4,8 +4,10 @@ from bs4 import BeautifulSoup
 import re
 import copy
 import nltk
+from nltk.tokenize import word_tokenize
 # nltk.download('punkt')
 from nltk.stem import PorterStemmer
+import math
 
 # UNCOMMENT THIS BASED ON WHOSE COMPUTER IS BEING USED
 
@@ -28,13 +30,12 @@ docid_counter = 0  # for naming docID
 def tokenizes(data):
     # tokenizes and stems with ntlk
     ps = PorterStemmer()
-    tokenizer = nltk.RegexpTokenizer(r"\w+")
-    tokens = tokenizer.tokenize(data)
+    tokens = word_tokenize(data)
 
     # removes words that shouldn't be considered
     copy_list = copy.deepcopy(tokens)
     for word in copy_list:
-        if len(word) < 3:
+        if len(word) < 2:
             tokens.remove(word)
         else:
             # checks if stemming
@@ -45,34 +46,21 @@ def tokenizes(data):
     return tokens
 
 
-def compute_tf(token, tokens):
-    # term_freq(t,d) = count of t in d / number of words in d
-    amount_of_token_in_tokens = tokens.count(token) * 1.0
-    length_of_tokens = len(tokens) * 1.0
-    return amount_of_token_in_tokens/length_of_tokens
-
-
-def compute_idf():
-    # inverse_doc_freq(t) = log(N/(df + 1))
-    return 0.0
-
-
-def compute_tfidf():
-    # TO-DO: formula is tf-idf(t, d) = tf(t, d) * log(N/(df + 1))
-    return 0.0
-
-
 def add_to_index(document_words, docid_counter):
     # value should document name/id token was found in
-    # and tf-idf score (need to compute)
+
     for word in document_words:
-        tfidf_score = 0.0
+        # calculate tf score, freq of token in an entire doc
+        freq_of_token = float(document_words.count(word))
+        amount_of_words = float(len(document_words))
+        tf_score = freq_of_token/amount_of_words
+
         if word not in inverse_index:
-            first_appearance = (docid_counter, tfidf_score)
+            first_appearance = (docid_counter, tf_score)
             inverse_index[word] = set()
             inverse_index[word].add(first_appearance)
         else:
-            inverse_index[word].add((docid_counter, tfidf_score))
+            inverse_index[word].add((docid_counter, tf_score))
 
 
 for subdir, dirs, files in os.walk(dev_directory):
@@ -84,7 +72,9 @@ for subdir, dirs, files in os.walk(dev_directory):
         try:
             soup = BeautifulSoup(open(json_file), 'html.parser')
             for text in soup.findAll(["title", "p", "b", re.compile('^h[1-6]$')]):
-                data = text.get_text().strip()
+                data = text.get_text(" ", strip=True)
+                data = data.lower()
+                data = re.sub('[^A-Za-z0-9]+', ' ', str(data))
                 alphanumeric_sequences += tokenizes(data)
             add_to_index(alphanumeric_sequences, docid_counter)
         except Exception as e:
@@ -94,6 +84,9 @@ for subdir, dirs, files in os.walk(dev_directory):
 # for deliverable 1
 deliverable_text = open('/Users/AreetaW/Desktop/deliverable.txt', 'w')
 for key, value in inverse_index.items():
+    # find inverse of (total number of docs/number of docs with token in it)
+    for v in value:
+        v[1] *= math.log(float(docid_counter)/float(len(inverse_index[key])))
     deliverable_text.write(str(key) + ":     " + str(value) + "\n")
 deliverable_text.close()
 
