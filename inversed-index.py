84a25cc6867c33abcf7291d2521b858f14820b78
ccervera1 <ccer29@hotmail.com>
2020-05-12 00:09:16 -0700

    Updated to include use of pandas for merging indexes

--- inversed-index.py
+++ inversed-index.py
@@ -10,16 +10,17 @@ import math
 import json
 import time
 import demjson
+import pandas
 
 
 # Kaeley:
 # dev_directory = '/Users/kaeleylenard/Documents/CS121-Spring2020/SearchEngine/DEV/www_ics_uci_edu'
 
 # Areeta:
-dev_directory = '/Users/AreetaW/Desktop/cs/cs-121/assignment3/DEV'
+# dev_directory = '/Users/AreetaW/Desktop/cs/cs-121/assignment3/DEV'
 
 # Cristian:
-# dev_directory = 'C:\Test\DEV'
+dev_directory = 'C:\Test\DEV'
 # dev_directory = 'C:\Test\custom'
 
 
@@ -36,6 +37,8 @@ def tokenizes(data):
     ps = PorterStemmer()
     for word in data:
         tokenized = re.sub('[^A-Za-z0-9]+', ' ', str(word))
+        tokenized = re.sub('[^A-Za-z0-9].', ' ', str(tokenized))
+        tokenized = tokenized.replace(' ', '')
         if len(tokenized) >= 2:
             tokens.append(ps.stem(tokenized))
     return tokens
@@ -73,14 +76,15 @@ def write_to_file():
     docid_counter = 0
 
     # Cristian:
-    # deliverable_text = open(f'C:\Test\info{index_count}.txt', 'w')
+    deliverable_text = open(f'C:\Test\info{index_count}.txt', 'w')
     # Kaeley:
     # deliverable_text = open(f'/Users/kaeleylenard/Desktop/info{index_count}.txt', 'w')
     # Areeta:
-    deliverable_text = open(f'/Users/AreetaW/Desktop/info{index_count}.txt', 'w')
+    # deliverable_text = open(f'/Users/AreetaW/Desktop/info{index_count}.txt', 'w')
 
     with deliverable_text as json_file:
-        json.dump(demjson.encode(inverse_index), json_file)
+        inverse_index = {k: list(v) for k, v in sorted(inverse_index.items())}  # Sorts dict by key
+        json.dump(inverse_index, json_file)
     deliverable_text.close()
     inverse_index.clear()
 
@@ -102,8 +106,8 @@ for subdir, dirs, files in os.walk(dev_directory):
         except Exception as e:
             print("error at:", e)
 
-
-# for deliverable 1
+'''
+# for deliverable 1    # I am not sure what this was for but it ended up here - Cristian
 deliverable_text = open('/Users/AreetaW/Desktop/deliverable.txt', 'w')
 for key, value in inverse_index.items():
     # find inverse of (total number of docs/number of docs with token in it)
@@ -111,40 +115,44 @@ for key, value in inverse_index.items():
         v[1] *= math.log(float(docid_counter)/float(len(inverse_index[key])))
     deliverable_text.write(str(key) + ":     " + str(value) + "\n")
 deliverable_text.close()
+'''
 
 word_count += len(inverse_index)
 index_count += 1
 total_docs += docid_counter
 
 # Cristian
-# deliverable_text = open(f'C:\Test\info{index_count}.txt', 'w')
+deliverable_text = open(f'C:\Test\info{index_count}.txt', 'w')
 # Kaeley
 # deliverable_text = open(f'/Users/kaeleylenard/Desktop/info{index_count}.txt', 'w')
 # Areeta
-deliverable_text = open(f'/Users/AreetaW/Desktop/info{index_count}.txt', 'w')
+#deliverable_text = open(f'/Users/AreetaW/Desktop/info{index_count}.txt', 'w')
 
 with deliverable_text as json_file:
-    json.dump(demjson.encode(inverse_index), json_file)
+    inverse_index = {k: list(v) for k, v in sorted(inverse_index.items())}
+    json.dump(inverse_index, json_file)
 deliverable_text.close()
 
 # Cristian
-# file_list = [f'C:\Test\info{x+1}.txt' for x in range(index_count)]
+file_list = [f'C:\Test\info{x+1}.txt' for x in range(index_count)]
 # Kaeley
 # file_list = [f'/Users/kaeleylenard/Desktop/info{x+1}.txt' for x in range(index_count)]
 # Areeta
-file_list = [f'/Users/AreetaW/Desktop/info{x+1}.txt' for x in range(index_count)]
+# file_list = [f'/Users/AreetaW/Desktop/info{x+1}.txt' for x in range(index_count)]
 
 # Cristian
 # with open('C:\Test\data.txt', 'w') as json_file:
 # Kaeley
 # with open('/Users/kaeleylenard/Desktop/data.txt', 'w') as json_file:
 # Areeta
-with open('/Users/AreetaW/Desktop/data.txt', 'w') as json_file:
-    for index in file_list:
-        with open(index) as file:
-            data = json.load(file)
-            data = demjson.decode(data)
-        json.dump(data, json_file)
+# with open('/Users/AreetaW/Desktop/data.txt', 'w') as json_file:
+
+bases = []    # Use of pandas to merge all json files alphabetically
+for file in file_list:
+    temp = pandas.read_json(file, orient='index')
+    bases.append(temp)
+result = pandas.concat(bases) # This should hold all json files as one big pandas dataframe
+result.to_csv("C:\Test\/finalindex.csv") # This will export it into excel
 
 # Statistics
 print("\nREPORT")
