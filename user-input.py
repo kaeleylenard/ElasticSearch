e6a014dfa3b76eb4f3d03d951e6a3819330aff7c
areetaw <areetaw@gmail.com>
2020-05-22 03:07:59 -0700

    issue w eval

--- user-input.py
+++ user-input.py
@@ -14,7 +14,8 @@ def find_rare_words(query):
 
     for word in query:
         if word not in stop_words:
-            rare_words.append(ps.stem(word))  # stem the word
+            # add stemmed word to list
+            rare_words.append(ps.stem(word))
 
     # for queries that contain only stopwords, such as "to be or not to be"
     # just return the entire query - will need to find documents that contain
@@ -29,50 +30,58 @@ def iterate_info_files(rare_query):
     Find docs with the query terms.
     present_docs: docs that contain ALL the query terms
     rare_query: updated stemmed query with rare terms and no stop words
-
-    posting:
-    word : “{(docID, score)}”
-
-    entire file:
-    {“0”: {word : “{(docID, score)}”}}
     """
 
-    present_docs = []  # holds all curr docs that match the words
-    found = 0
+    queries_docs = []
 
     # Areeta
-    # final_text_index = "/Users/AreetaW/Desktop/finaltextindex.txt"
+    final_text_index = "/Users/AreetaW/Desktop/final_text_index.txt"
+    final_url_index = "/Users/AreetaW/Desktop/final_url_index.txt"
+    dev_directory = '/Users/AreetaW/Desktop/cs/cs-121/assignment3/DEV'
     # Kaeley
-    final_text_index = "/Users/kaeleylenard/Desktop/final_text_index.txt"
-    final_url_index = "/Users/kaeleylenard/Desktop/info_urls1.txt"
-    with open(final_url_index) as file:
-        url_response = json.loads(file.read())
+    # final_text_index = "/Users/kaeleylenard/Desktop/final_text_index.txt"
+    # final_url_index = "/Users/kaeleylenard/Desktop/final_info_urls.txt"
+    # dev_directory = '/Users/kaeleylenard/Documents/CS121-Spring2020/Assignment3/DEV'
     # Cristian
-    # final_text_index = ''
+    # final_text_index = "/Users/kaeleylenard/Desktop/final_text_index.txt"
+    # final_url_index = "/Users/kaeleylenard/Desktop/final_info_urls.txt"
+    # dev_directory = 'C:\Test\DEV'
+    # dev_directory = 'C:\Test\custom'
 
-    with open(final_text_index) as file:
-        text_response = json.loads(file.read())
-        # format for postings {(docID, score)} is a string
-        for (word, posting) in text_response['0'].items():
+    # load dict of all urls in index
+    with open(final_url_index) as url_file:
+        url_response = json.loads(url_file.read())
+
+    # load dict of inverted index
+    with open(final_text_index) as text_file:
+        text_response = json.loads(text_file.read())
+        for (word, posting) in text_response['all_pages'].items():
             if word in rare_query:
-                for (docID, score) in eval(posting):
-                    for(ID, json_file) in url_response.items():
-                        print(json_file)
-                        # next steps: get url from json file, ranking, MAKE THIS MORE EFFICIENT AAAH
-"""
-# Areeta's:
+                # add appearance of word in format: (word url, word, and td-idf score)
 
-with open("/Users/AreetaW/Desktop/finaltextindex.txt") as file:
-    response = json.loads(file.read())
-    for (word, postings) in response['0'].items():
-        if rare_words[found] == word:
-            found += 1
-            # took out quotes and curly brackets on both sides 
-            # need to figure out how to unstringify it
-            posting_info = posting_info[1:-1]
-            
-            # add posting_info to present docs
-"""
+                # current issue: not able to eval posting because multiple sets
+                list_of_postings = eval("[" + posting + "]")
+                print(list_of_postings)
+                for (docID, score) in list_of_postings:
+                    url_path = dev_directory + url_response['0'][docID]
+                    url_response = json.loads((open(url_path)).read())
+                    queries_docs.append((url_response['url'], word, score))
+
+    url_file.close()
+    text_file.close()
+
+    if len(queries_docs) > 1:
+        return find_intersection(queries_docs)
+    return queries_docs
+
+
+def find_intersection(queries_docs):
+    # HELLO I AM HAVING AN ISSUE DETERMINING HOW TO FIND INTERSECTIONS IF IT'S MULTIPLE WORDS
+    # like if word has 3 urls and another word has 3 urls but only 1 match up
+    intersections = 0
+    while intersections < 4:
+        for (word, url) in queries_docs.values():
+            print("asdfsa")
 
 
 def retrieval_component(query):
@@ -81,14 +90,11 @@ def retrieval_component(query):
     query_words: stemmed, rare query terms
     returned_docs: docs that contain ALL the query_words
     """
-    query_words = find_rare_words(query)
-    returned_docs = iterate_info_files(query_words)
+    rare_query = find_rare_words(query)
+    returned_docs = iterate_info_files(rare_query)
 
-    # return only the top five matches
-    # for doc in returned_docs[:5]:
-        # buffer the document to get the first line which contains the URL
-        # print the URL
-        # print(doc)
+    # print only the top five matches based on tf-idf score
+    print(returned_docs.sort(key=lambda x: x[1], reverse=True)[0:5])
 
 
 if __name__ == "__main__":
